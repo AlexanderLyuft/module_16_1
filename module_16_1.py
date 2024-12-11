1204f52fa499a9bdbcc9e86da7269ac5f272d401
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-12-11 22:56:26 +0700

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,25 @@
+            # Домашнее задание по теме "Основы Fast Api и маршрутизация"
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "Главная страница"
+
+@app.get("/user/admin")
+def read_admin():
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+def read_user(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("/user")
+def read_user_info(username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
+
+
